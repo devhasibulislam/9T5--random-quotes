284d9790bfc6d3e97a03b5f42ad2f585c317c871
hasibulislam999 <hasib143sl@gmail.com>
2023-01-21 21:54:42 +0600

    display random quote driver code init

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ * Title: Display Random Quote
+ * Description: At each run display a random quote from a quote data file
+ * Author: Hasibul Islam
+ * Date: 21/01/2023
+ */
+
+// external import
+const fs = require("fs");
+
+// internal import
+const randomDigit = require("./library/RandomDigit");
+
+const data = fs
+  .readFileSync(`${__dirname}/data/Quotes.txt`, "utf8")
+  .replace(/\r\n/g, "\n")
+  .split("\n");
+
+if (data) {
+  const index = randomDigit(randomDigit(data.length));
+  console.log(index + ":", data[index]);
+} else {
+  console.log(data);
+}
